1aedd196a87ffc5214693351e0acc5ab91581a83
Jocelyn Naarden <tzionone@gmail.com>
2022-07-14 04:37:09 -0400

    Assignment done

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +128,81 @@ def game_hash
   }
 end
 
-# Write code here
+def get_players(field)
+  game_hash.each do |team, data| 
+    data
+    # binding.pry
+  end
+end
+
+# methods with *** have to do with just players
+def get_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def get_team(team_name)
+  game_hash.find do |team_field, team_stats|
+    team_stats[:team_name] == team_name
+  end
+end
+
+#num_points_scored // takes player returns score
+def num_points_scored(player_name)
+  player_stats(player_name)[:points]
+end
+  
+  # ***show_size // takes player returns shoe size
+def shoe_size(player_name)
+  player_stats(player_name)[:shoe]
+end
+
+  #team_colors // takes teamName returns color
+def team_colors(team)
+  get_team(team)[1][:colors]
+end
+
+  #team_names // returns Array of names
+def team_names
+  team_array = []
+  game_hash.each do |team_field, team_stats|
+    team_array << team_stats[:team_name]
+  end
+    team_array
+end
+  
+# player_numbers of team
+def player_numbers(team_name)
+  player_num_array = []
+  get_team(team_name)[1][:players].each do |player|
+    player_num_array << player[:number]
+  end
+  player_num_array
+end
+
+  # *** player_stats // takes player name returns player stats
+def player_stats(player_name)
+  get_players.find do |player|
+    player[:player_name] == player_name
+  end
+end
+# *** big_shoe_rebounds // takes player with largest shoe returns rebound
+def big_shoe_rebounds
+  # sorted array makes biggest shoe last
+  sorted_array =  get_players.sort_by{ |player| player[:shoe]}
+  sorted_array.last[:rebounds]
+end
+## ADDITIONAL PRACTICE
+# *** most_points_scored // which player has most points
+def most_points_scored
+  # sorted array makes biggest shoe last
+  sorted_array =  get_players.sort_by{ |player| player[:points]}
+  sorted_array.last[:player_name]
+end
+#winning_team // add all players points return team
+# *** player_with_longest_name // returns player with ...
+# *** long_name_steals_a_ton? // compare to all other players steals
+
+# get_players
+# puts team_names
+# num_points_scored("Jason Terry")
+puts most_points_scored #Ben Gordon
